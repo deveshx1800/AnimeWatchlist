be87c5f6e27b0c926275fd5179b29d9993f5a0a4
devesh kumar <idevesh18@gmail.com>
2026-02-17 21:40:27 +0530

    Initial Commit

---
+++ AnimeWatchlist/AnimeWatchlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeWatchlistApp.swift
+//  AnimeWatchlist
+//
+//  Created by Devesh on 17/02/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimeWatchlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
